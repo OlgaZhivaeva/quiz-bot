7fb1c473873871388e67ed3e8e189a24fece6139
OlgaZhivaeva <olga.zhiv@yandex.ru>
2025-01-08 21:48:15 +0300

    Add redis to vk_bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,17 +1,19 @@
 import vk_api as vk
 import re
 import random
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.keyboard import VkKeyboard
 from vk_api.utils import get_random_id
 from environs import Env
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from redis_db import r
 from tg_bot import logger
 from questions_and_answers import questions_and_answers
 
 
 def start(event, vk_api, keyboard):
     """Send a message to start the quiz."""
+    user_id = str(event.user_id)
     vk_api.messages.send(
         user_id=event.user_id,
         random_id=get_random_id(),
@@ -19,12 +21,14 @@ def start(event, vk_api, keyboard):
         message="Привет! Нажмите 'Новый вопрос' для начала викторины\n"
                 " Для отмены нажмите 'Завершить викторину'"
     )
+    r.set(f"user:{user_id}:score", "0")
 
 
-def handle_new_question_request(user_data, event, vk_api, keyboard):
+def handle_new_question_request(event, vk_api, keyboard):
     """Handles request for a new question."""
+    user_id = str(event.user_id)
     question = random.choice(list(questions_and_answers.keys()))
-    user_data[event.user_id]['current_question'] = question
+    r.set(f"user:{user_id}:current_question", question)
     print(f'вопрос:{question}')
     print(f'ответ:{questions_and_answers[question]}')
     vk_api.messages.send(
@@ -35,9 +39,10 @@ def handle_new_question_request(user_data, event, vk_api, keyboard):
     )
 
 
-def handle_solution_attempt(user_data, event, vk_api, keyboard):
+def handle_solution_attempt(event, vk_api, keyboard):
     """Handles user's attempt to answer a question."""
-    current_question = user_data[event.user_id]['current_question']
+    user_id = str(event.user_id)
+    current_question = r.get(f"user:{user_id}:current_question")
     correct_answer = questions_and_answers[current_question]
     smart_correct_answer = re.search(r'^[^(^.]+', correct_answer).group().lower().strip().strip("'\"")
     user_answer = re.search(r'^[^.]+', event.text).group().lower().strip()
@@ -50,7 +55,8 @@ def handle_solution_attempt(user_data, event, vk_api, keyboard):
             keyboard=keyboard.get_keyboard(),
             message='Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос"'
         )
-        user_data[event.user_id]['score'] += 1
+        score = int(r.get(f"user:{user_id}:score"))
+        r.set(f"user:{user_id}:score", str(score + 1))
 
     else:
          vk_api.messages.send(
@@ -61,9 +67,10 @@ def handle_solution_attempt(user_data, event, vk_api, keyboard):
         )
 
 
-def show_score(user_data, event, vk_api, keyboard):
+def show_score(event, vk_api, keyboard):
     """Shows user's current score."""
-    score = user_data[event.user_id].get('score', 0)
+    user_id = str(event.user_id)
+    score = r.get(f"user:{user_id}:score")
 
     vk_api.messages.send(
         user_id=event.user_id,
@@ -73,20 +80,23 @@ def show_score(user_data, event, vk_api, keyboard):
     )
 
 
-def end(user_data, event, vk_api, keyboard):
+def end(event, vk_api, keyboard):
     """Handles end of quiz"""
-    score = user_data[event.user_id].get('score', 0)
+    user_id = str(event.user_id)
+    score = r.get(f"user:{user_id}:score")
     vk_api.messages.send(
         user_id=event.user_id,
         random_id=get_random_id(),
         message=f"Викторина завершена.\nВаш счет {score}."
     )
-    user_data.clear()
+    r.delete(f"user:{user_id}:score")
+    r.delete(f"user:{user_id}:current_question")
 
 
-def handle_solution_give_up(user_data, event, vk_api, keyboard):
+def handle_solution_give_up(event, vk_api, keyboard):
     """Handles user giving up on a question."""
-    current_question = user_data[event.user_id].get("current_question")
+    user_id = str(event.user_id)
+    current_question = r.get(f"user:{user_id}:current_question")
     correct_answer = questions_and_answers[current_question]
     vk_api.messages.send(
         user_id=event.user_id,
@@ -118,20 +128,21 @@ def main():
     logger.info('vk-бот запущен')
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            user_id = event.user_id
-            if user_id not in user_data:
-                user_data[user_id] = {'current_question': None, 'score': 0}
-                start(event, vk_api, keyboard)
-            elif event.text == 'Новый вопрос':
-                handle_new_question_request(user_data, event, vk_api, keyboard)
+            if event.text == 'Новый вопрос':
+                handle_new_question_request(event, vk_api, keyboard)
             elif event.text == 'Сдаться':
-                handle_solution_give_up(user_data, event, vk_api, keyboard)
+                handle_solution_give_up(event, vk_api, keyboard)
             elif event.text == 'Мой счет':
-                show_score(user_data, event, vk_api, keyboard)
+                show_score(event, vk_api, keyboard)
             elif event.text == 'Завершить викторину':
-                end(user_data, event, vk_api, keyboard)
+                end(event, vk_api, keyboard)
             else:
-                handle_solution_attempt(user_data, event, vk_api, keyboard)
+                user_id = str(event.user_id)
+                current_question = r.get(f"user:{user_id}:current_question")
+                if current_question is None:
+                    start(event, vk_api, keyboard)
+                else:
+                    handle_solution_attempt(event, vk_api, keyboard)
 
 
 if __name__ == '__main__':
