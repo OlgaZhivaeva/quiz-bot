b191b36736e4ca497dfa00ee7683f9c1433bb265
OlgaZhivaeva <olga.zhiv@yandex.ru>
2025-01-11 02:56:25 +0300

    Rename def questions_and_answers to def get_questions_and_answers

--- quiz_data_parser.py
+++ quiz_data_parser.py
@@ -7,26 +7,8 @@ from environs import Env
 logger = logging.getLogger(__name__)
 
 
-def load_questions_and_answers(file_path):
-    """Downloads and parses questions and answers."""
-    try:
-        with open(file_path, "r", encoding="KOI8-R") as file:
-            file_contents = file.read()
-
-        pattern = re.compile(r'Вопрос \d+:\s*(.*?)\n\nОтвет:\s*(.*?)\n\n', flags=re.DOTALL)
-        matches = pattern.findall(file_contents)
-        questions_and_answers = {question: answer for question, answer in matches}
-        return questions_and_answers
-    except FileNotFoundError:
-        logger.error(f'Файл {file_path} не найден.')
-        return {}
-    except Exception as e:
-        logger.error(f'Ошибка при загрузке файла: {e}')
-        return {}
-
-
 def get_questions_and_answers():
-    """Get questions and answers dictionary."""
+    """Parses questions and answers into a dictionary"""
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
     )
@@ -35,8 +17,21 @@ def get_questions_and_answers():
     env.read_env()
     questions_file_name = env.str('QUESTIONS_FILE', '1vs1200.txt')
     questions_file_path = Path(__file__).parent / questions_file_name
-    questions_and_answers = load_questions_and_answers(questions_file_path)
-    return questions_and_answers
+
+    try:
+        with open(questions_file_path, "r", encoding="KOI8-R") as file:
+            file_contents = file.read()
+
+        pattern = re.compile(r'Вопрос \d+:\s*(.*?)\n\nОтвет:\s*(.*?)\n\n', flags=re.DOTALL)
+        matches = pattern.findall(file_contents)
+        questions_and_answers = {question: answer for question, answer in matches}
+        return questions_and_answers
+    except FileNotFoundError:
+        logger.error(f'Файл {questions_file_path} не найден.')
+        return {}
+    except Exception as e:
+        logger.error(f'Ошибка при загрузке файла: {e}')
+        return {}
 
 
 def main():
